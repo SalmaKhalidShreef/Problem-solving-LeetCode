06e3ff049d2dfff3ae789c828e54a482906a8994
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-02-25 14:49:09 +0200

    Time: 0 ms (100.00%), Space: 41.5 MB (35.75%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -1,46 +1,28 @@
-class Solution {
+public class Solution {
     public int compareVersion(String version1, String version2) {
-        String [] v1 = version1.split("\\.");
-        String [] v2 = version2.split("\\.");
-        int i =0;
-        int j =0;
-        int s1= 0;
-        int s2=0;
-        boolean flag = false;
-        while(i<v1.length && j <v2.length){
-            int ii=0;
-            int jj=0;
-            s1=0;
-            s2=0;
-                s1+=Integer.parseInt(v1[i]);
+        int i = 0, j = 0;
+        int currentVersion1, currentVersion2;
+        while(i < version1.length() || j < version2.length()) {
+            currentVersion1 = 0;
+            while(i < version1.length() && version1.charAt(i) != '.'){
+                currentVersion1 = currentVersion1 * 10 + (version1.charAt(i) - '0');
+                i++;
+            }
+            
+            currentVersion2 = 0;
+            while(j < version2.length() && version2.charAt(j) != '.'){
+                currentVersion2 = currentVersion2 * 10 + (version2.charAt(j) - '0');
+                j++;
+            }
+            
+            if(currentVersion1 > currentVersion2)
+                return 1;
+            if(currentVersion2 > currentVersion1)
+                return -1;
             i++;
-                s2+=Integer.parseInt(v2[j]);
-            j++;
-            
-            if(s1!=s2){
-                flag = true;
-                System.out.println("hena");
-
-                break;
-            }
-            
-
-        }
-         if(flag){
-             int res =s1>s2?1:s1<s2?-1:0;
-             return res;  
-            }
-        while(i<v1.length){
-            s1+=Integer.parseInt(v1[i]);
-
-            i++; 
-        }
-        while(j<v2.length){
-            s2+=Integer.parseInt(v2[j]);
             j++;
         }
-int res =s1>s2?1:s1<s2?-1:0;
-             return res;  
         
+        return 0;
     }
 }
