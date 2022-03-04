f36636408a36b96fb339931544f4b7d52221b348
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-03-04 21:48:25 +0200

    Time: 8 ms (59.82%), Space: 46.7 MB (46.58%) - LeetHub

---
+++ 799-champagne-tower/799-champagne-tower.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[][] pyramid=new double[query_row+2][query_row+2];
+        pyramid[0][0]=poured;
+        for(int i=0;i<=query_row;i++){
+            for(int j=0;j<=i;j++){
+                if(pyramid[i][j]>1){
+                    pyramid[i+1][j]+=(double)(pyramid[i][j]-1)/2;
+                    pyramid[i+1][j+1]+=(double)(pyramid[i][j]-1)/2;
+                    pyramid[i][j]=1;
+                }
+            }
+        }
+        return pyramid[query_row][query_glass];
+        
+    }
+    
+}
