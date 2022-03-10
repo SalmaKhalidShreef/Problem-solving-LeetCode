c63f1e0aef6b68d9a43946260e1c8a3975d1b3c7
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-03-10 22:27:44 +0200

    Time: 3 ms (73.88%), Space: 48 MB (23.92%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,58 +10,24 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int tmp =0;
+        int carry =0;
         ListNode result = new ListNode(-100);
         ListNode restmp=result;
-        while(l1!= null && l2!= null){
-            int l3 = l1.val+l2.val+tmp;
-            tmp=0;
-            if(l3<10){
-                restmp.next=new ListNode(l3);
-            }
-            else
-            {
-                restmp.next=new ListNode(l3%10);
-                tmp=l3/10;
-            }
-            restmp=restmp.next;
-            l1=l1.next;
-            l2=l2.next;
-        }
-        if(l1!=null){
-            while(tmp>0 && l1!=null){
-                int l3=l1.val+tmp;
-                tmp=0;
-                if(l3<10){
-                    restmp.next=new ListNode(l3);
-                }
-                else{
-                   restmp.next=new ListNode(l3%10);
-                    tmp=l3/10; 
-                }
+        while(l1!= null || l2!= null){
+            int val1= (l1!=null)?l1.val:0;
+            int val2=(l2!=null)?l2.val:0;
+            int l3 = val1+val2+carry;
+            carry=(l3>=10)?l3/10:0;
+            int val3=l3%10;
+            restmp.next = new ListNode(val3);            
+            if(l1!=null)
                 l1=l1.next;
-                restmp=restmp.next;
-            }
-            restmp.next=l1;
+            if(l2!=null)
+                l2=l2.next;    
+            restmp=restmp.next;
         }
-        if(l2!=null){
-            while(tmp>0 && l2!=null){
-                int l3=l2.val+tmp;
-                tmp=0;
-                if(l3<10){
-                    restmp.next=new ListNode(l3);
-                }
-                else{
-                   restmp.next=new ListNode(l3%10);
-                    tmp=l3/10; 
-                }                l2=l2.next;
-                restmp=restmp.next;
-            }
-            restmp.next=l2;
-        }
-        if(tmp>0){
-            restmp.next=new ListNode(tmp);
-        }
+        if(carry>0)
+            restmp.next=new ListNode(carry);
         return result.next;
         
     }
