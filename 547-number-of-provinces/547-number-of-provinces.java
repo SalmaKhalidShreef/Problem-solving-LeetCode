cd6d1780c2ded6b3e63867e29b19c391b4746824
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-03-13 11:55:31 +0200

    Time: 1 ms (94.43%), Space: 53.5 MB (54.69%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,25 +1,24 @@
 class Solution {
-    private static int provinces =0;
-    private static int[][] graph;
-    private static boolean []visited;
     public int findCircleNum(int[][] isConnected) {
-        provinces=0;
-        graph = isConnected;
-        visited=new boolean[isConnected.length];
-        for(int city=0;city< graph.length;city++){
-            if(!visited[city]){
-                provinces++;
-                dfs(city);
+        int connected=0;
+        int[] visited = new int[isConnected.length];
+        for(int i =0;i<isConnected.length;i++){
+                if(visited[i]==0){
+                dfs(isConnected,i,visited);
+                connected++;}
+            
+        }
+    return connected;}
+    public void dfs(int[][]grid,int i ,int[] visited){
+        for(int j=0;j<grid.length;j++){
+            if (grid[i][j]==1 && visited[j]==0){
+                visited[j]=1;
+                dfs(grid,j,visited);
+
             }
         }
-        return provinces;
-    }
-    private void dfs(int city){
-        visited[city]=true;
-        for(int city2=0;city2<graph[city].length;city2++){
-            if(city!=city2 && graph[city][city2]==1 && !visited[city2]){
-                dfs(city2);
-            }
         }
-    }
+        
+        
+    
 }
