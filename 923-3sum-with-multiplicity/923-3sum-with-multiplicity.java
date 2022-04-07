517dc6584ac0aa8168cf84a2a5d5b0cbffb604ab
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-04-07 03:00:20 +0200

    Time: 487 ms (15.92%), Space: 53.6 MB (15.10%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,51 +1,18 @@
 class Solution {
     public int threeSumMulti(int[] A, int target) {
         int MOD = 1_000_000_007;
-        long[] count = new long[101];
-        for (int x: A)
-            count[x]++;
-
-        long ans = 0;
-
-        // All different
-        for (int x = 0; x <= 100; ++x)
-            for (int y = x+1; y <= 100; ++y) {
-                int z = target - x - y;
-                if (y < z && z <= 100) {
-                    ans += count[x] * count[y] * count[z];
-                    ans %= MOD;
+        long res=0;
+        for(int i =0;i<A.length;i++){
+            int[] occ = new int[101];
+            for(int j =i+1;j<A.length;j++){
+                int t = target-A[i]-A[j];
+                if(t>=0 && t<=100 && occ[t]>0 ){
+                    res+=occ[t];
+                    res=res%MOD;
                 }
-            }
-
-        // x == y != z
-        for (int x = 0; x <= 100; ++x) {
-            int z = target - 2*x;
-            if (x < z && z <= 100) {
-                ans += count[x] * (count[x] - 1) / 2 * count[z];
-                ans %= MOD;
+                occ[A[j]]++;
             }
         }
-
-        // x != y == z
-        for (int x = 0; x <= 100; ++x) {
-            if (target % 2 == x % 2) {
-                int y = (target - x) / 2;
-                if (x < y && y <= 100) {
-                    ans += count[x] * count[y] * (count[y] - 1) / 2;
-                    ans %= MOD;
-                }
-            }
-        }
-
-        // x == y == z
-        if (target % 3 == 0) {
-            int x = target / 3;
-            if (0 <= x && x <= 100) {
-                ans += count[x] * (count[x] - 1) * (count[x] - 2) / 6;
-                ans %= MOD;
-            }
-        }
-
-        return (int) ans;
+        return (int) res;
     }
 }
