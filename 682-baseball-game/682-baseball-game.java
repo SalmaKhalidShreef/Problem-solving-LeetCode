bcdf8d8660a745d5a4e3d21d5cac69d865522a8d
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-04-11 01:43:53 +0200

    Time: 4 ms (54.23%), Space: 42.7 MB (28.91%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int calPoints(String[] ops) {
+        Stack<Integer> s = new Stack();
+        for(String str :ops){
+            if(Character.isDigit(str.charAt(0)))
+                s.push(Integer.parseInt(str));
+            else if(str.charAt(0)=='-')
+                s.push(0-Integer.parseInt(str.substring(1,str.length())));
+            else if(str.equals("+")){
+                int x = s.pop();
+                int y = s.pop();
+                int z = x+y;
+                s.push(y);
+                s.push(x);
+                s.push(z);
+            }
+            else if(str.equals("D"))
+                s.push(s.peek()*2);
+            else
+                s.pop();
+        }
+        int res=0;
+        while(!s.isEmpty())
+            res+=s.pop();
+        return res;
+    }
+}
