9e1bcaf249fee98a1fa5480ef23a13d14a2baadd
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-04-14 02:29:40 +0200

    Time: 3 ms (98.22%), Space: 43.6 MB (80.93%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -3,25 +3,19 @@ class Solution {
         
         int r=grid.length;
         int c=grid[0].length;        
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        for(int i =0;i<r;i++){
-            List<Integer> list = new ArrayList<Integer>();
-            for(int j =0;j<c;j++)
-                list.add(0);
-            result.add(list);
-        }
-        
+        int[][] temp = new int[r][c];
         for (int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                int newFlatIdx = (i*c+j+ k) % (r*c);
-                int newi = newFlatIdx / c;
-                int newj = newFlatIdx % c;
-                result.get(newi).set(newj,grid[i][j]);
+                int newIndex = (i*c+j+ k) % (r*c);
+                int newr = newIndex / c;
+                int newc = newIndex % c;
+                temp[newr][newc] = grid[i][j];
             }
         }
         
+        
 
-        return result;
+         return (List)Arrays.asList(temp);
         
     }
 }
