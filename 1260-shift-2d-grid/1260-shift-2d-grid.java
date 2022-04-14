2c49b2310227f27a7b1aadb5ad40ec50c2bf2cae
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-04-14 02:25:30 +0200

    Time: 6 ms (82.09%), Space: 42.9 MB (94.41%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -2,31 +2,24 @@ class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         
         int r=grid.length;
-        int c=grid[0].length;
-        int oned[] = new int [r*c];
-
-        
-        int temp[][] = new int [r][c];
+        int c=grid[0].length;        
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for(int i =0;i<r;i++){
+            List<Integer> list = new ArrayList<Integer>();
+            for(int j =0;j<c;j++)
+                list.add(0);
+            result.add(list);
+        }
         
         for (int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                int newIndex = (i*c+j+ k) % oned.length;
-                int newr = newIndex / c;
-                int newc = newIndex % c;
-                temp[newr][newc] = grid[i][j];
+                int newFlatIdx = (i*c+j+ k) % (r*c);
+                int newi = newFlatIdx / c;
+                int newj = newFlatIdx % c;
+                result.get(newi).set(newj,grid[i][j]);
             }
         }
         
-        // Prepare Result in List
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-
-        for(int i = 0; i < r; i++) {
-            List<Integer> list = new ArrayList<Integer>();
-            for(int j = 0; j < c; j++) {
-                list.add(temp[i][j]);
-            }
-            result.add(list);
-        }
 
         return result;
         
