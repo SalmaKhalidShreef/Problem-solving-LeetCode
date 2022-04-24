09d1723172918ce99499915f717218507ef7fe47
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-04-24 14:51:10 +0200

    Time: 200 ms (24.16%), Space: 102.3 MB (41.30%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,6 +1,6 @@
 class UndergroundSystem {
     HashMap<Integer,CheckIn> checkIns;
-    HashMap<String,Trip> trips;
+    HashMap<String,Map<String,Trip>> trips;
     public UndergroundSystem() {
         checkIns= new HashMap();
         trips=new HashMap();
@@ -13,17 +13,36 @@ class UndergroundSystem {
     
     public void checkOut(int id, String stationName, int t) {
         CheckIn check = checkIns.get(id);
-        String tripId=check.city+"/"+stationName;
-        Trip trip = trips.getOrDefault(tripId,new Trip(check.city,stationName,t-check.time));
+        Trip trip;
+        Map innerMap=null;
+        if(trips.containsKey(check.city)){
+            if(trips.get(check.city).containsKey(stationName)){
+                innerMap=trips.get(check.city);
+                trip=(Trip)innerMap.get(stationName);
+            }
+            else{
+                trip=new Trip(t-check.time);
+                innerMap=trips.get(check.city);
+            }
+        }
+        else
+            trip = new Trip(t-check.time);
+        
+        if(innerMap==null)
+            innerMap=new HashMap();
+        
         trip.addTrip(t-check.time);
-        trips.put(tripId,trip);
+        
+        innerMap.put(stationName,trip);
+        trips.put(check.city,innerMap);
+
         checkIns.remove(id);
+
     }
     
     public double getAverageTime(String startStation, String endStation) {
-        String tripId= startStation+"/"+endStation;        
-        return trips.get(tripId).avgTime;
-        //return 0;
+
+        return trips.get(startStation).get(endStation).avgTime;
     }
 
 }
@@ -40,14 +59,10 @@ class CheckIn{
 
 
 class Trip{
-    String id;
-    String from;
-    String to;
     double tripsCnt;
     int times;
     double avgTime;
-    public Trip(String from , String to,int tripTime){
-        this.id=from+"/"+to;
+    public Trip(int tripTime){
         this.tripsCnt=0;
         this.times=0;
         this.avgTime=0;
@@ -59,9 +74,14 @@ class Trip{
     }
 }
 /**
+        System.out.println(startStation+" "+endStation);
+        System.out.println(trips.get(startStation));
+        System.out.println(trips.get(startStation).get(endStation));
  * Your UndergroundSystem object will be instantiated and called as such:
  * UndergroundSystem obj = new UndergroundSystem();
  * obj.checkIn(id,stationName,t);
  * obj.checkOut(id,stationName,t);
  * double param_3 = obj.getAverageTime(startStation,endStation);
+ ["UndergroundSystem","checkIn","checkOut","getAverageTime","checkIn","checkOut","getAverageTime","checkIn","getAverageTime","checkIn","getAverageTime","getAverageTime","checkOut"]
+[[],[37043,"K2618O72",29],[37043,"U1DTINDT",39],["K2618O72","U1DTINDT"],[779975,"K2618O72",112],[779975,"CN3K6CYM",157],["K2618O72","U1DTINDT"],[706901,"K2618O72",221],["K2618O72","CN3K6CYM"],[18036,"K2618O72",258],["K2618O72","U1DTINDT"],["K2618O72","CN3K6CYM"],[18036,"U1DTINDT",293]]
  */
