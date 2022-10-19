64a491bd3fa395b28619af8abc4c5ec152c7357c
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-10-19 21:17:10 +0200

    Time: 18 ms (10.82%), Space: 45.6 MB (24.62%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String,Integer> hm = new HashMap();
+        for(String word:words){
+            hm.put(word,hm.getOrDefault(word,0)+1);
+        }
+         PriorityQueue<Pair<String, Integer>> pq =  
+                              new PriorityQueue<>(new PairComparator());
+        for(Map.Entry<String,Integer> entry : hm.entrySet()){
+            pq.add(new Pair<>(entry.getKey(), entry.getValue()));
+            if(pq.size()> k)
+                pq.remove();
+        }
+        
+        List<String> result = new ArrayList();
+        while(!pq.isEmpty())
+            result.add(0,pq.remove().getKey());
+        
+        return result;
+    }
+    class PairComparator implements Comparator<Pair<String,Integer>>{
+            public int compare(Pair<String,Integer> p1, Pair<String,Integer> p2) {
+                if(Integer.compare(p1.getValue(), p2.getValue())== 0)
+                    return p2.getKey().compareTo(p1.getKey());
+                return Integer.compare(p1.getValue(), p2.getValue());   
+                }
+        }
+}
