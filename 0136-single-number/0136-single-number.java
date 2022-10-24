9ce95f4b59c98936228d01842cc47d9b604a96ff
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-10-24 20:13:03 +0200

    Time: 5 ms (57.31%), Space: 50 MB (76.51%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int tmp =nums[0];
+        for(int i =1;i<nums.length;i++)
+            tmp ^= nums[i];
+        return tmp;
+    }
+}
