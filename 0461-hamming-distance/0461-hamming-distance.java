007de8876375a1e4efc7f4e62979dd254f1331fa
Salma Khalid Shreef <salmakh200070@gmail.com>
2022-10-24 20:08:02 +0200

    Time: 0 ms (100.00%), Space: 39.1 MB (94.79%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,13 +1,6 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int res =0;
-        for(int i =0;i<31;i++){
-            if(getBit(x,i) != getBit(y,i))
-                res++;
-        }
-        return res;
-    }
-    private int getBit(int num, int i){
-        return(num & (1<<i));
+        int tmp = x ^ y;
+        return Integer.bitCount(tmp);
     }
 }
