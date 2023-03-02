987db93f90eb30e2bd03d7468047d6a5163202eb
Salma Khalid Shreef <salmakh200070@gmail.com>
2023-03-02 19:36:08 +0200

    Time: 1 ms (98.42%), Space: 42 MB (57.67%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int compress(char[] chars) {
+        int res = 0;
+        int i = 0;
+        while(i<chars.length)
+        {
+            int cnt = 1;
+            while(i+cnt < chars.length && chars[i+cnt]==chars[i])
+            {
+                cnt++;
+            }
+            chars[res++] = chars[i];
+            if(cnt > 1)
+            {
+                for(char c : Integer.toString(cnt).toCharArray())
+                {
+                    chars[res++] = c;
+                }
+            }
+            i += cnt;
+        }
+        return res;
+    }
+}
